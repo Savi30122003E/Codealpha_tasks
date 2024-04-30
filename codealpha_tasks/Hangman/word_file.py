8ff58cd480cb95c5f310e24f1857198dc7020284
Savitha E <168509204+Savi30122003E@users.noreply.github.com>
2024-04-30 11:20:23 +0530

    Update word_file.py

--- codealpha_tasks/Hangman/word_file.py
+++ codealpha_tasks/Hangman/word_file.py
@@ -1,1 +1,6 @@
-words=["apple","banana","cranberry","dragonfruit","elderberry","fig","grape","honeydewmelon","imbe","jackfruit","kiwifruit","lemon","mango","nectarine","orange","papaya","quince","raspberry","strawberry","tangerine","uglifruit","victoriaplum","watermelon","xigua","yangmei","zucchini"]
+words=["apple","banana","cranberry","dragonfruit",
+       "elderberry","fig","grape","honeydewmelon",
+       "imbe","jackfruit","kiwifruit","lemon","mango",
+       "nectarine","orange","papaya","quince","raspberry",
+       "strawberry","tangerine","uglifruit","victoriaplum",
+       "watermelon","xigua","yangmei","zucchini"]
